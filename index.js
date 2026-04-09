55f4601f3502704d65531f3e9e81beea3460a345
Omar Ramy <omarramy.e12@gmail.com>
2026-04-09 17:21:29 +0200

    update

---
+++ index.js
@@ -0,0 +1,30 @@
+const {program} = require("commander");
+
+program
+.name("expense-tracker")
+.description("A simple expense Tracker CLI")
+.version("1.0.0");
+
+// ** Add : command - decription - requriedoption"description"/amount - option"category" and general - action(options with require add)
+program
+.command("add")
+.description(" Add a new expense")
+.requiredOption("--description <description>", "Expense description")
+.requiredOption("--amount <amount>", "Expense Amount")
+.option("--category <category>", "Expense Category", "General")
+.action((options) => {
+    require("./commands/add")
+});
+
+// update
+program
+.command("update")
+.description("Update an Expense")
+.requiredOption("--id <Id>", "Expense ID")
+.option("--description <description>", "New Description")
+.option("--amount <amount>", "New Amount")
+.option("--Category <category>", "New Category")
+.action((option) => {
+    require("./commands/update")
+});
+
